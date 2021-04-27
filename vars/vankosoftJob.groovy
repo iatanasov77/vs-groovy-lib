5778b272e48c3f702193504491eb68faba068e9c
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-27 07:31:03 +0300

    First Commit

---
+++ vars/vankosoftJob.groovy
@@ -0,0 +1,9 @@
+
+////////////////////////////////////////////////////
+// https://gist.github.com/eeichinger/9761870
+////////////////////////////////////////////////////
+def getGitTags( $repo ) {
+    def gettags = ( "git ls-remote -t -h ${repo} refs/tags/*" ).execute()
+
+    return gettags.text.readLines().collect { it.split()[1].replaceAll( 'refs/tags/', '' )  }.unique()
+}
