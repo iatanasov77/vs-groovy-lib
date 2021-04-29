495b5eb4648f83cec31f7575aaec4e2b1cd219f5
Ivan I. Atanasov <i.atanasov77@gmail.com>
2021-04-29 11:07:50 +0300

    Fix Syntax Error

--- vars/vankosoftJob.groovy
+++ vars/vankosoftJob.groovy
@@ -6,6 +6,6 @@ def getGitTags( String repo ) {
     def gettags = ( "git ls-remote -t -h ${repo} refs/tags/*" ).execute()
     def tags    = gettags.text.readLines().collect { it.split()[1].replaceAll( 'refs/tags/', '' )  }.unique()
 
-    //return tags
-    return tags.removeAll { it.contains( '^{}' ) }.unique()
+    tags.removeAll { it.contains( '^{}' ) }
+    return tags
 }
